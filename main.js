00eb913209b3c1be4989facdcaffcb945fabdae0
Francesco Musi <francesco.musi03@gmail.com>
2019-02-27 23:12:08 +0100

    pagina web iniziata

---
+++ main.js
@@ -0,0 +1,39 @@
+const fetch  = require('node-fetch')
+
+let change_image_button = document.getElementById('change_image')
+let image = document.querySelector('img')
+
+const change_image = () => {
+
+  let image_src = image.getAttribute('src')
+
+  if(image_src === 'images/first_image.png') {
+    image.setAttribute ('src','images/second_image.png')
+  } else {
+    image.setAttribute ('src','images/first_image.png')
+  }
+
+}
+
+
+change_image_button.onclick = function() {
+	change_image()
+}
+
+image.onclick = function() {
+	change_image()
+}
+
+
+let request_button = document.getElementById('request_button')
+let text_request = document.getElementById()
+
+request_button.onclick = function() {
+	fetch('https://api.paninidellospalla.it/info')
+	.then(res => {
+		document.getElementById('text_request').innerHTML = res.json()
+	})
+	
+}
+
+
